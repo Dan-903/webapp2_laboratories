0e474c34b166d40cf41f010e092872821f0a7449
Daniel Latina <166882758+Dan-903@users.noreply.github.com>
2024-05-17 18:38:36 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php
@@ -0,0 +1,33 @@
+<?php
+$name = "Daniel Latina";
+$age = 20;
+$address = "Sampaloc Apalit, Pampanga";
+// $hobbies = "Learning about other cultures, biking, sleep";
+// $pet_peeves = "Slow internet Connection";
+
+
+echo $name . "</br>";
+echo $age . "</br>";
+echo $address . "</br>";
+echo $hobbies . "</br>";
+echo $pet_peeves . "</br>" . "</br>";
+
+define("name", "Daniel Latina");
+define("age", "20");
+define("address", "Sampaloc Apalit, Pampanga");
+define("hobbies", "Learning about other cultures and biking");
+define("pet_peeves", "Slow internet Connection");
+
+print(name . "</br>");
+print(address . "</br>");
+print(age . "</br>");
+print(hobbies . "</br>");
+print(pet_peeves . "</br>" . "</br>");
+
+var_dump(name, address, age, hobbies, pet_peeves)
+
+/* Slow internet is frustrating to deal with,
+having to wait long periods before something loads up... 
+it's inconvenient */
+
+?>
